cae71ca61a034f76cd347cfb8a0c008fdf715fb4
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-11-14 03:44:29 -0500

    t

--- myLambda/index.js
+++ myLambda/index.js
@@ -1,25 +1,53 @@
-const aws = require('aws-sdk');
-const ses = new aws.SES({
+const AWS = require('aws-sdk')
+const ses = new AWS.SES({
     region: 'us-east-1'
-});
+})
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'})
 
-exports.handler = (event)=>{
-    // console.log(event.Records[0].Sns.Message);
-    const email = {
-        Destination: {
-            ToAddresses: [event.Records[0].Sns.Message]
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: "Reset password"
+exports.handler = function (event, context) {
+    const email = event.Records[0].Sns.Message
+
+    const getItemObject = {
+        TableName: 'csye6225',
+        Key: {
+            'id': {S: email}
+        }
+    }
+
+    ddb.getItem(getItemObject, function (err, data) {
+            if (data.Item === undefined) {
+                const putItemObject = {
+                    TableName: 'csye6225',
+                    Item: {
+                        id: {S: email},
+                        token: {S: context.awsRequestId},
+                        ttl: {N: (Math.floor(Date.now() / 1000) + 60 * 20).toString()}
+                    }
                 }
-            },
-            Subject: {
-                Data: "Reset password"
+                ddb.putItem(putItemObject, function () {
+                    }
+                )
+
+                const emailObject = {
+                    Destination: {
+                        ToAddresses: [email]
+                    },
+                    Message: {
+                        Body: {
+                            Text: {
+                                Data: "Reset password"
+                            }
+                        },
+                        Subject: {
+                            Data: "Reset password"
+                        }
+                    },
+                    Source: "reset-password@csye6225-fall2018-zhangzhiyo.me"
+                }
+                ses.sendEmail(emailObject, function () {
+                    }
+                )
             }
-        },
-        Source: "reset-password@csye6225-fall2018-zhangzhiyo.me"
-    };
-    ses.sendEmail(email, ()=>{});
-};
+        }
+    )
+}

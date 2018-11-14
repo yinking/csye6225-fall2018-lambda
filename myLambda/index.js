24709a4afd6c6668e0b697c5f69047b4f5a85db4
fourthzi5 <fourthzi5@localhost.localdomain>
2018-11-14 15:45:40 -0500

    Add new index.js
    Please enter the commit message for your changes. Lines starting

---
+++ myLambda/index.js
@@ -0,0 +1,61 @@
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'us-east-1'})
+const ses = new AWS.SES()
+const dynamoDB = new AWS.DynamoDB()
+const route53domains = new AWS.Route53Domains()
+
+exports.handler = function (event, context) {
+    
+    route53domains.listDomains({MaxItems: 1}, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);  
+        return data;
+    });
+    const email = event.Records[0].Sns.Message
+
+    const getItemObject = {
+        TableName: 'csye6225',
+        Key: {
+            'id': {S: email}
+        }
+    };
+
+    dynamoDB.getItem(getItemObject, function (err, data) {
+        if (data.Item === undefined) {
+            const putItemObject = {
+                TableName: 'csye6225',
+                Item: {
+                    id: {S: email},
+                    token: {S: context.awsRequestId},
+                    ttl: {N: (Math.floor(Date.now() / 1000) + 60 * 20).toString()}
+                }
+            }
+            dynamoDB.putItem(putItemObject, function () {
+                }
+            )
+            
+            route53domains.listDomains({MaxItems: 1}, function (err, data) {
+                console.log(data)
+                /*const emailObject = {
+                    Destination: {
+                        ToAddresses: [email]
+                    },
+                    Message: {
+                        Body: {
+                            Text: {
+                                Data: "Reset password"
+                            }
+                        },
+                        Subject: {
+                            Data: "Reset password"
+                        }
+                    },
+                    Source: "reset-password@" + data.Domains[0].DomainName
+                }
+                ses.sendEmail(emailObject, function () {
+                    
+                });*/
+            });
+        }
+    })
+}

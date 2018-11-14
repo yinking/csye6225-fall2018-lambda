6610ebde03cd62b90888c96fc3c513fcfd1d9d2c
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-11-14 18:47:45 -0500

    t

--- myLambda/index.js
+++ myLambda/index.js
@@ -1,55 +1,51 @@
 const AWS = require('aws-sdk')
-AWS.config.update({region: 'us-east-1'})
+AWS.config.update({ region: 'us-east-1' })
 const ses = new AWS.SES()
 const dynamoDB = new AWS.DynamoDB()
-const route53domains = new AWS.Route53Domains()
+const route53 = new AWS.Route53()
 
-exports.handler = function (event, context) {
+exports.handler = (event, context) => {
     const email = event.Records[0].Sns.Message
 
     const getItemObject = {
         TableName: 'csye6225',
         Key: {
-            'id': {S: email}
+            'id': { S: email }
         }
     }
 
-    dynamoDB.getItem(getItemObject, function (err, data) {
-            if (data.Item === undefined) {
-                const putItemObject = {
-                    TableName: 'csye6225',
-                    Item: {
-                        id: {S: email},
-                        token: {S: context.awsRequestId},
-                        ttl: {N: (Math.floor(Date.now() / 1000) + 60 * 20).toString()}
-                    }
-                }
-                dynamoDB.putItem(putItemObject, function () {
-                    }
-                )
-
-                route53domains.listDomains(params, function (err, data) {
-                    const emailObject = {
-                        Destination: {
-                            ToAddresses: [email]
-                        },
-                        Message: {
-                            Body: {
-                                Text: {
-                                    Data: "Reset password"
-                                }
-                            },
-                            Subject: {
-                                Data: "Reset password"
-                            }
-                        },
-                        Source: "reset-password@" + data.Domains[0].DomainName
-                    }
-                    ses.sendEmail(emailObject, function () {
-                        }
-                    )
-                });
+    dynamoDB.getItem(getItemObject, (err, data) => {
+        if (data.Item === undefined) {
+        const putItemObject = {
+            TableName: 'csye6225',
+            Item: {
+                id: { S: email },
+                token: { S: context.awsRequestId },
+                ttl: { N: (Math.floor(Date.now() / 1000) + 60 * 20).toString() }
             }
         }
-    )
+        dynamoDB.putItem(putItemObject, () => {})
+
+        route53.listHostedZones({}, (err, data) => {
+            const domainName=data.HostedZones[0].Name
+            const emailObject = {
+                Destination: {
+                    ToAddresses: [email]
+                },
+                Message: {
+                    Body: {
+                        Text: {
+                            Data: "Reset password"
+                        }
+                    },
+                    Subject: {
+                        Data: "Reset password"
+                    }
+                },
+                Source: "reset-password@" + domainName.substring(0,domainName.length-1)
+            }
+            ses.sendEmail(emailObject, () => {})
+    });
+    }
+})
 }
